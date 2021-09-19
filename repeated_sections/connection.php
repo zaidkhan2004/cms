5ced3b0480e4c7bb3029497a4ab16180c1aa5956
zaidkhan2004 <90557999+zaidkhan2004@users.noreply.github.com>
2021-09-19 22:19:57 +0500

    Update connection.php

--- repeated_sections/connection.php
+++ repeated_sections/connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root","","cms_db"); 
+$conn = mysqli_connect("172.105.37.124", "qvvtxdhrye","WZ9r3pXZSN","qvvtxdhrye"); 
 // Establishing Connection with Server..
 
 //$db = mysqli_select_db("cl28-a-moodl-smn", $conn); 
